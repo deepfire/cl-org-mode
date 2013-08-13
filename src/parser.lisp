fe8d12076eceb9f4f648230428a8d5d34b9c2c8f
Samium Gromoff <_deepfire@feelingofgreen.ru>
2013-08-14 03:31:02 +0400

    Tracer: conditionalise tracing on *DEBUG-MODE*

--- src/parser.lisp
+++ src/parser.lisp
@@ -104,15 +104,21 @@
     (format t "~A, line:col ~D:~D, next char: ~S~%" name line col char)))
 
 
+(defun call-with-maybe-tracing (tracep mark fn)
+  (if tracep
+      (named-seq*
+       (with-posn-info? (char line col)
+         (format t "~S ? (next ~S, line:col ~D:~D)~%" mark char line col))
+       (<- res (funcall fn))
+       (progn
+         (format t "~S ok - ~S~%" mark res)
+         res))
+      (funcall fn)))
+
+(defvar *debug-mode* nil)
+
 (defmacro c? (x)
-  (with-gensyms (res char line col)
-    `(named-seq*
-      (with-posn-info? (,char ,line ,col)
-        (format t "~S ? (next ~S, line:col ~D:~D)~%" ',x ,char ,line ,col))
-      (<- ,res ,x)
-      (progn
-        (format t "~S ok - ~S~%" ',x ,res)
-        ,res))))
+  `(call-with-maybe-tracing *debug-mode* ',x (lambda () ,x)))
 
 ;;;
 ;;; Tools
