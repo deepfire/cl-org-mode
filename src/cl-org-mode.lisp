77cb70915d58860d1bf47f7a9449b2cff6c4cf55
Samium Gromoff <_deepfire@feelingofgreen.ru>
2013-08-15 03:48:52 +0400

    Restore READ-ORG-FILE to its pristine state

--- src/cl-org-mode.lisp
+++ src/cl-org-mode.lisp
@@ -87,8 +87,8 @@ then stick it in the default node"
 
 (defun read-org-file (pathname)
   (let ((node (make-instance 'org-file :pathname pathname)))
-    (setf node (megaparser (alexandria:read-file-into-string pathname)))
-    node))
+    (alexandria:with-input-from-file (stream pathname) 
+      (read-parent-node node stream))))
 
 (defclass delimited-node (org-parent-node)
   ((opening-delimiter :initarg :opening-delimiter :accessor node.opening-delimiter :initform nil)
