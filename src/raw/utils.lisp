90860cf66afb0de375022e5ad2ed5d8bde1cfd8f
Jingtao Xu <jingtaozf@gmail.com>
2018-02-27 14:29:25 +0800

    fix exception when org document has empty section.

--- src/raw/utils.lisp
+++ src/raw/utils.lisp
@@ -10,9 +10,27 @@
             (collect elt into no))
         (finally (return (values yes no)))))
 
+(defmacro dotree ((name tree &optional ret-val) &body body)
+  "Evaluate BODY with NAME bound to every element in TREE. Return RET-VAL."
+  (with-unique-names (traverser list list-element)
+    `(progn
+       (labels ((,traverser (,list)
+                  (dolist (,list-element ,list)
+                    (if (consp ,list-element)
+                        (,traverser ,list-element)
+                        (let ((,name ,list-element))
+                          ,@body)))))
+         (,traverser ,tree)
+         ,ret-val))))
+
 ;; from pergamum
 (defun strconcat (xs)
-  (apply #'concatenate 'string xs))
+  (let ((*print-pretty* nil)
+        (*print-circle* nil))
+    (with-output-to-string (stream)
+      (dotree (str xs)
+        (when str
+          (princ str stream))))))
 
 ;; from pergamum
 (defun strconcat* (&rest xs)
